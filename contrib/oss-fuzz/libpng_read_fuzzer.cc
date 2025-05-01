dca6ab23cabb5a572f75f2fa8204502341628939
David Pulido <dpulidoc99@gmail.com>
2025-05-01 18:48:07 +0200

    enable palette checker

--- contrib/oss-fuzz/libpng_read_fuzzer.cc
+++ contrib/oss-fuzz/libpng_read_fuzzer.cc
@@ -151,6 +151,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     PNG_CLEANUP
     return 0;
   }
+  
+  /* ------------ EXTRA COVERAGE: palette index validation ------------- */
+  /* This enables the run-time check guarded by
+    #ifdef PNG_CHECK_FOR_INVALID_INDEX_SUPPORTED in pngtrans.c          */
+  png_set_check_for_invalid_index(png_ptr, /*allowed=*/1);
+
 
   // Reading.
   png_read_info(png_handler.png_ptr, png_handler.info_ptr);
