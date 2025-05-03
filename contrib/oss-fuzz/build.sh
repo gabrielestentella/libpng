9a99d5b447224951f45c8837af6eae3c80c864b5
David Pulido <dpulidoc99@gmail.com>
2025-05-03 23:18:30 +0200

    swapped builders

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -22,7 +22,7 @@
 # 3. Build zlib alongside libpng
 ################################################################################
 
-#export CFLAGS="$CFLAGS -DPNG_SIMPLIFIED_READ_SUPPORTED -DPNG_SIMPLIFIED_WRITE_SUPPORTED"
+export CFLAGS="$CFLAGS -DPNG_SIMPLIFIED_READ_SUPPORTED -DPNG_SIMPLIFIED_WRITE_SUPPORTED"
 export CXXFLAGS="$CXXFLAGS -DPNG_SIMPLIFIED_READ_SUPPORTED -DPNG_SIMPLIFIED_WRITE_SUPPORTED"
 
 
@@ -42,7 +42,7 @@ make -j$(nproc) libpng16.la
 
 # build libpng_write_fuzzer.
 $CXX $CXXFLAGS -std=c++11 -I. \
-     $SRC/libpng/contrib/oss-fuzz/libpng_auto_fuzzer.cc \
+     $SRC/libpng/contrib/oss-fuzz/libpng_write_fuzzer.cc \
      -o $OUT/libpng_write_fuzzer \
      -lFuzzingEngine .libs/libpng16.a -lz
 
