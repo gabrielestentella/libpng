5b6cd5e450396c79d1104239fd712f9a30f3eb99
David Pulido <dpulidoc99@gmail.com>
2025-05-03 22:40:09 +0200

    reduced write harness

--- contrib/oss-fuzz/libpng_write_fuzzer.cc
+++ contrib/oss-fuzz/libpng_write_fuzzer.cc
@@ -43,71 +43,18 @@
     png_handler.end_info_ptr = nullptr; \
   }
 
-struct BufState {
-  const uint8_t* data;
-  size_t bytes_left;
-};
-
-struct PngObjectHandler {
-  png_infop info_ptr = nullptr;
-  png_structp png_ptr = nullptr;
-  png_infop end_info_ptr = nullptr;
-  png_voidp row_ptr = nullptr;
-  BufState* buf_state = nullptr;
-
-  ~PngObjectHandler() {
-    if (row_ptr)
-      png_free(png_ptr, row_ptr);
-    if (end_info_ptr)
-      png_destroy_read_struct(&png_ptr, &info_ptr, &end_info_ptr);
-    else if (info_ptr)
-      png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
-    else
-      png_destroy_read_struct(&png_ptr, nullptr, nullptr);
-    delete buf_state;
-  }
-};
-
-void user_read_data(png_structp png_ptr, png_bytep data, size_t length) {
-  BufState* buf_state = static_cast<BufState*>(png_get_io_ptr(png_ptr));
-  if (length > buf_state->bytes_left) {
-    png_error(png_ptr, "read error");
-  }
-  memcpy(data, buf_state->data, length);
-  buf_state->bytes_left -= length;
-  buf_state->data += length;
-}
-
-void* limited_malloc(png_structp, png_alloc_size_t size) {
-  // libpng may allocate large amounts of memory that the fuzzer reports as
-  // an error. In order to silence these errors, make libpng fail when trying
-  // to allocate a large amount. This allocator used to be in the Chromium
-  // version of this fuzzer.
-  // This number is chosen to match the default png_user_chunk_malloc_max.
-  if (size > 8000000)
-    return nullptr;
-
-  return malloc(size);
-}
-
-void default_free(png_structp, png_voidp ptr) {
-  return free(ptr);
-}
-
-static const int kPngHeaderSize = 8;
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size < 5) return 0; // need at least a few bytes for dimensions
   png_image image;
   memset(&image, 0, sizeof(image));
   image.version = PNG_IMAGE_VERSION;
-  //Use first bytes to construct small width/height to avoid huge allocs
+  //use first bytes to construct small width/height to avoid huge allocs
   image.width = data[0] + 1;
   image.height = data[1] + 1;
   int colormode = data[2] % 3;
   //pick a color format: 0=RGBA, 1=GRAY, 2=RGBA palette (just for variety)
   image.format = (colormode == 1 ? PNG_FORMAT_GRAY : PNG_FORMAT_RGBA);
-  //Allocate pixel buffer (for simplicity, assume 8-bit components)
+  //Allocate pixel buffer (assume 8-bit components)
   size_t pixel_size = PNG_IMAGE_SIZE(image);
   if (pixel_size > size - 3 || pixel_size == 0) return 0;
   //Use remaining fuzz data as pixel bytes
@@ -117,9 +64,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   png_alloc_size_t out_size = 0;
   if (!png_image_write_to_memory(&image, &out_buf, &out_size, 0, pixels, 0, NULL)) {
       png_image_free(&image);
-      return 0; // writing failed (possibly invalid combination)
+      return 0; // writing failed
   }
-  // Free image memory
+  //free image memory
   png_image_free(&image);
   free(out_buf);
   return 0;
