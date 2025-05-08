c55766f08f62b51b89a5782f4436ea5b494a43fd
Octave Charrin <octave.charrin@gmail.com>
2025-05-08 17:23:01 +0200

    modified harness

--- contrib/oss-fuzz/libpng_improved_read_fuzzer.cc
+++ contrib/oss-fuzz/libpng_improved_read_fuzzer.cc
@@ -1,16 +1,14 @@
 // libpng_improved_read_fuzzer.cc
 // Enhanced to cover critical functions in pngread.c
-
 // Copyright 2017-2018 Glenn Randers-Pehrson
 // Copyright 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that may
-// be found in the LICENSE file https://cs.chromium.org/chromium/src/LICENSE
+// Use of this source code is governed by a BSD-style license.
 
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <algorithm>
 #include <vector>
 #include <random>
 
@@ -49,14 +47,7 @@ struct PngObjectHandler {
   BufState* buf_state = nullptr;
 
   ~PngObjectHandler() {
-    if (row_ptr)
-      png_free(png_ptr, row_ptr);
-    if (end_info_ptr)
-      png_destroy_read_struct(&png_ptr, &info_ptr, &end_info_ptr);
-    else if (info_ptr)
-      png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
-    else
-      png_destroy_read_struct(&png_ptr, nullptr, nullptr);
+    PNG_CLEANUP
     delete buf_state;
   }
 };
@@ -72,14 +63,8 @@ void user_read_data(png_structp png_ptr, png_bytep data, size_t length) {
 }
 
 void* limited_malloc(png_structp, png_alloc_size_t size) {
-  // libpng may allocate large amounts of memory that the fuzzer reports as
-  // an error. In order to silence these errors, make libpng fail when trying
-  // to allocate a large amount. This allocator used to be in the Chromium
-  // version of this fuzzer.
-  // This number is chosen to match the default png_user_chunk_malloc_max.
-  if (size > 4000000) // Made shorter for stricter memory control
+  if (size > 4000000)
     return nullptr;
-
   return malloc(size);
 }
 
@@ -88,7 +73,6 @@ void default_free(png_structp, png_voidp ptr) {
 }
 
 static int user_chunk_callback(png_structp png_ptr, png_unknown_chunkp chunk) {
-  // Randomize handling to test different paths
   static std::random_device rd;
   static std::mt19937 gen(rd());
   std::uniform_int_distribution<> dis(0, 2);
@@ -103,7 +87,7 @@ static int user_chunk_callback(png_structp png_ptr, png_unknown_chunkp chunk) {
   } else if (choice == 1) {
     return -1; // Simulate error
   }
-  return 0; // Default: save chunk
+  return 0; // Save chunk
 }
 
 static const int kPngHeaderSize = 8;
@@ -124,7 +108,7 @@ void apply_random_transforms(png_structp png_ptr, uint32_t seed) {
   if (dis(gen)) png_set_swap_alpha(png_ptr);
 }
 
-// Test simplified read API with colormap and gamma variations
+// Test simplified read API with colormap
 int test_simplified_read(const uint8_t* data, size_t size, uint32_t seed) {
   png_image image;
   memset(&image, 0, sizeof(image));
@@ -135,12 +119,10 @@ int test_simplified_read(const uint8_t* data, size_t size, uint32_t seed) {
     return 0;
   }
 
-  // Randomly choose format and transformations
   std::mt19937 gen(seed);
   std::uniform_int_distribution<> dis(0, 3);
   int format_choice = dis(gen);
 
-  // Vary format to trigger colormap and different encodings
   switch (format_choice) {
     case 0:
       image.format = PNG_FORMAT_GRAY | PNG_FORMAT_FLAG_COLORMAP;
@@ -157,23 +139,15 @@ int test_simplified_read(const uint8_t* data, size_t size, uint32_t seed) {
       break;
   }
 
-  // Randomly set gamma to trigger set_file_encoding
-  if (dis(gen)) {
-    png_set_gAMA(image.opaque->png_ptr, image.opaque->info_ptr, 0.45455); // sRGB
-  } else if (dis(gen)) {
-    png_set_gAMA(image.opaque->png_ptr, image.opaque->info_ptr, 1.0); // Linear
-  } else {
-    png_set_gAMA(image.opaque->png_ptr, image.opaque->info_ptr, 0.8); // Custom
-  }
-
   std::vector<png_byte> buffer(PNG_IMAGE_SIZE(image));
   std::vector<png_byte> colormap;
   if (image.format & PNG_FORMAT_FLAG_COLORMAP) {
     colormap.resize(image.colormap_entries * 4); // RGBA colormap
   }
 
-  png_color background = {128, 128, 128}; // Gray background
-  if (!png_image_finish_read(&image, &background, buffer.data(), 0, colormap.empty() ? nullptr : colormap.data())) {
+  png_color background = {128, 128, 128};
+  if (!png_image_finish_read(&image, &background, buffer.data(), 0,
+                             colormap.empty() ? nullptr : colormap.data())) {
     png_image_free(&image);
     return 0;
   }
@@ -211,21 +185,14 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     return 0;
   }
 
-  // Use part of input as seed for randomization
   uint32_t seed = size >= 4 ? *(uint32_t*)data : 0;
+  std::mt19937 gen(seed);
+  std::uniform_int_distribution<> dis(0, 2);
 
-  // Test simplified read API
   test_simplified_read(data, size, seed);
 
-  // Original sequential read path
   PngObjectHandler png_handler;
-  png_handler.png_ptr = nullptr;
-  png_handler.row_ptr = nullptr;
-  png_handler.info_ptr = nullptr;
-  png_handler.end_info_ptr = nullptr;
-
-  png_handler.png_ptr = png_create_read_struct
-    (PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+  png_handler.png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
   if (!png_handler.png_ptr) {
     return 0;
   }
@@ -241,16 +208,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     PNG_CLEANUP
     return 0;
   }
-  
-  // Use a custom allocator that fails for large allocations to avoid OOM.
-  png_set_mem_fn(png_handler.png_ptr, nullptr, limited_malloc, default_free);
 
+  png_set_mem_fn(png_handler.png_ptr, nullptr, limited_malloc, default_free);
   png_set_crc_action(png_handler.png_ptr, PNG_CRC_QUIET_USE, PNG_CRC_QUIET_USE);
 #ifdef PNG_IGNORE_ADLER32
   png_set_option(png_handler.png_ptr, PNG_IGNORE_ADLER32, PNG_OPTION_ON);
 #endif
 
-  // Setting up reading from buffer.  
   png_handler.buf_state = new BufState();
   png_handler.buf_state->data = data + kPngHeaderSize;
   png_handler.buf_state->bytes_left = size - kPngHeaderSize;
@@ -267,18 +231,24 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   png_set_keep_unknown_chunks(png_handler.png_ptr, PNG_HANDLE_CHUNK_ALWAYS, nullptr, 0);
 #endif
 
+  // Set gamma for sequential read
+  if (dis(gen)) {
+    png_set_gAMA(png_handler.png_ptr, png_handler.info_ptr, 0.45455); // sRGB
+  } else if (dis(gen)) {
+    png_set_gAMA(png_handler.png_ptr, png_handler.info_ptr, 1.0); // Linear
+  } else {
+    png_set_gAMA(png_handler.png_ptr, png_handler.info_ptr, 0.8); // Custom
+  }
+
   png_read_info(png_handler.png_ptr, png_handler.info_ptr);
 
-  // reset error handler to put png_deleter into scope.
   if (setjmp(png_jmpbuf(png_handler.png_ptr))) {
     PNG_CLEANUP
     return 0;
   }
 
   png_uint_32 width, height;
-  int bit_depth, color_type, interlace_type, compression_type;
-  int filter_type;
-
+  int bit_depth, color_type, interlace_type, compression_type, filter_type;
   if (!png_get_IHDR(png_handler.png_ptr, png_handler.info_ptr, &width,
                     &height, &bit_depth, &color_type, &interlace_type,
                     &compression_type, &filter_type)) {
@@ -286,28 +256,21 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     return 0;
   }
 
-  // This is going to be too slow. Reduced to 50M
   if (width && height > 50000000 / width) {
     PNG_CLEANUP
     return 0;
   }
 
-    // Enable MNG intrapixel differencing for RGB/RGBA
 #ifdef PNG_MNG_FEATURES_SUPPORTED
   if (color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_RGB_ALPHA) {
-    png_handler.png_ptr->mng_features_permitted |= PNG_FLAG_MNG_FILTER_64;
-    png_handler.png_ptr->filter_type = PNG_INTRAPIXEL_DIFFERENCING;
+    png_set_mng_features(png_handler.png_ptr, PNG_FLAG_MNG_FILTER_64);
   }
 #endif
 
-  // Apply random transformations
   apply_random_transforms(png_handler.png_ptr, seed);
-
   int passes = png_set_interlace_handling(png_handler.png_ptr);
- 
   png_read_update_info(png_handler.png_ptr, png_handler.info_ptr);
 
-  // Choose reading method randomly
   int read_method = dis(gen);
   if (read_method == 0) { // Use png_read_row
     png_handler.row_ptr = png_malloc(
@@ -317,12 +280,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       return 0;
     }
 
-  for (int pass = 0; pass < passes; ++pass) {
-    for (png_uint_32 y = 0; y < height; ++y) {
-      png_read_row(png_handler.png_ptr,
-                   static_cast<png_bytep>(png_handler.row_ptr), nullptr);
+    for (int pass = 0; pass < passes; ++pass) {
+      for (png_uint_32 y = 0; y < height; ++y) {
+        png_read_row(png_handler.png_ptr,
+                     static_cast<png_bytep>(png_handler.row_ptr), nullptr);
+      }
     }
-  }
   } else if (read_method == 1) { // Use png_read_rows
     std::vector<png_bytep> rows(height);
     png_size_t rowbytes = png_get_rowbytes(png_handler.png_ptr, png_handler.info_ptr);
@@ -345,7 +308,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       return 0;
     }
 
-    png_uint_32 rows_to_read = height / 2 + 1; // Read partial chunks
+    png_uint_32 rows_to_read = height / 2 + 1;
     for (png_uint_32 y = 0; y < height; y += rows_to_read) {
       png_uint_32 num_rows = std::min(rows_to_read, height - y);
       png_read_rows(png_handler.png_ptr, rows.data() + y, nullptr, num_rows);
@@ -383,29 +346,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     }
   }
 
-  // Test png_read_png to cover png_free_data
   if (dis(gen)) {
     test_png_read_png(png_handler, seed);
   }
-  
+
   png_read_end(png_handler.png_ptr, png_handler.end_info_ptr);
 
   PNG_CLEANUP
-  
-#ifdef PNG_SIMPLIFIED_READ_SUPPORTED
-  // Simplified READ API
-  png_image image;
-  memset(&image, 0, (sizeof image));
-  image.version = PNG_IMAGE_VERSION;
-
-  if (!png_image_begin_read_from_memory(&image, data, size)) {
-    return 0;
-  }
-
-  image.format = PNG_FORMAT_RGBA;
-  std::vector<png_byte> buffer(PNG_IMAGE_SIZE(image));
-  png_image_finish_read(&image, NULL, buffer.data(), 0, NULL);
-#endif
-
   return 0;
 }
