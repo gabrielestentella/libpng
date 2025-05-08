27edb534800ef3ff85869c290729d7574e4b49ab
gabrielestentella <gabriele.stentella@gmail.com>
2025-05-09 01:29:23 +0200

    cript

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -38,34 +38,15 @@ autoreconf -f -i
 make -j$(nproc) clean
 make -j$(nproc) libpng16.la
 
-# Build the libpng library with fuzzing support
-# autoreconf -f -i
-# ./configure --with-libpng-prefix=OSS_FUZZ_ \
-#     --enable-hardware-optimizations=no \
-#     --enable-shared=no \
-#     --enable-static=yes \
-#     CPPFLAGS="-DPNG_ARM_NEON_OPT=0 -DPNG_INTEL_SSE_OPT=0"
-# make -j$(nproc) clean
-# make -j$(nproc) libpng16.la
-
-
 # build libpng_read_fuzzer.
 $CXX $CXXFLAGS -std=c++11 -I. \
      $SRC/libpng/contrib/oss-fuzz/libpng_read_fuzzer.cc \
      -o $OUT/libpng_read_fuzzer \
      -lFuzzingEngine .libs/libpng16.a -lz
 
-# build pngtrans_fuzzer.
-# $CXX $CXXFLAGS -std=c++11 -I. \
-#     $SRC/libpng/contrib/oss-fuzz/pngtrans_fuzzer.cc \
-#     -o $OUT/pngtrans_fuzzer \
-#     -lFuzzingEngine .libs/libpng16.a -lz
-
 # add seed corpus.
 find $SRC/libpng -name "*.png" | grep -v crashers | \
      xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
-# find $SRC/libpng -name "*.png" | grep -v crashers | \
-#      xargs zip $OUT/pngtrans_fuzzer_seed_corpus.zip
 
 cp $SRC/libpng/contrib/oss-fuzz/*.dict \
      $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
