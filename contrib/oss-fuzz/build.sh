dc66385fdc9a2b97e9f1f76b830b8aec6df758d4
Octave Charrin <octave.charrin@gmail.com>
2025-05-13 00:39:33 +0200

    fix 1

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -22,7 +22,7 @@
 # 3. Build zlib alongside libpng
 ################################################################################
 
-export CXXFLAGS="$CXXFLAGS -DPNG_SIMPLIFIED_READ_SUPPORTED -DPNG_SIMPLIFIED_WRITE_SUPPORTED"
+export CXXFLAGS="$CXXFLAGS"
 
 # Disable logging via library build configuration control.
 cat scripts/pnglibconf.dfa | \
