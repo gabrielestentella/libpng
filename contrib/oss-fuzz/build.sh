a9aea55c317114334ac6d9f3332a594d0c8bfa0b
Octave Charrin <octave.charrin@gmail.com>
2025-05-13 00:36:11 +0200

    modularized the test getter

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -1,64 +1,51 @@
 #!/bin/bash -eu
-#
-# build.sh for libpng on OSS-Fuzz
-#
 
+# Copyright 2017-2018 Glenn Randers-Pehrson
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Revisions by Glenn Randers-Pehrson, 2017:
+# 1. Build only the library, not the tools (changed "make -j$(nproc) all" to
+#     "make -j$(nproc) libpng16.la").
+# 2. Disabled WARNING and WRITE options in pnglibconf.dfa.
+# 3. Build zlib alongside libpng
 ################################################################################
-# Enforce write support at compile time
-################################################################################
+
 export CXXFLAGS="$CXXFLAGS -DPNG_SIMPLIFIED_READ_SUPPORTED -DPNG_SIMPLIFIED_WRITE_SUPPORTED"
-# export CPPFLAGS="$CPPFLAGS -DPNG_WRITE_SUPPORTED -DPNG_WRITE_TRANSFORMS_SUPPORTED" 
 
-################################################################################
-# Patch pnglibconf.dfa
-#  * Disable STDIO and WARNING (unchanged)
-#  * LEAVE 'WRITE' ALONE so encoder symbols stay in the library
-################################################################################
+# Disable logging via library build configuration control.
 cat scripts/pnglibconf.dfa | \
-  sed -e "s/option STDIO/option STDIO disabled/" \
-      -e "s/option WARNING /option WARNING disabled/" \
+  sed -e 's/option STDIO/option STDIO disabled/' \
+      -e 's/option WARNING /option WARNING disabled/' \
 > scripts/pnglibconf.dfa.temp
 mv scripts/pnglibconf.dfa.temp scripts/pnglibconf.dfa
 
-################################################################################
-# Build the static libpng with write support
-################################################################################
+# build the libpng library.
 autoreconf -f -i
 ./configure --with-libpng-prefix=OSS_FUZZ_
-make -j"$(nproc)" clean
-make -j"$(nproc)" libpng16.la               # produces .libs/libpng16.a
+make -j$(nproc) clean
+make -j$(nproc) libpng16.la
 
-################################################################################
-# Build fuzzers
-################################################################################
-
-$CXX $CXXFLAGS -std=c++11 -I. \
-     $SRC/libpng/contrib/oss-fuzz/libpng_read_fuzzer.cc \
-     -o $OUT/libpng_read_fuzzer \
-     -lFuzzingEngine .libs/libpng16.a -lz
-
-$CXX $CXXFLAGS -std=c++11 -I. \
-     -DPNG_READ_UNKNOWN_CHUNKS_SUPPORTED \
-     $SRC/libpng/contrib/oss-fuzz/libpng_unknown_chunk_fuzzer.cc \
-     -o $OUT/libpng_unknown_chunk_fuzzer \
-     -lFuzzingEngine .libs/libpng16.a -lz
-
+# Build libpng_improved_read_fuzzer with getter.h included
 $CXX $CXXFLAGS -std=c++11 -I. \
      $SRC/libpng/contrib/oss-fuzz/libpng_improved_read_fuzzer.cc \
      -o $OUT/libpng_improved_read_fuzzer \
      -lFuzzingEngine .libs/libpng16.a -lz
 
-################################################################################
-# Package seed corpora and dictionaries (unchanged)
-################################################################################
+# add seed corpus.
 find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip -q $OUT/libpng_read_fuzzer_seed_corpus.zip
-
-find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip -q $OUT/libpng_unknown_chunk_fuzzer_seed_corpus.zip
-
-find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip -q $OUT/libpng_improved_read_fuzzer_seed_corpus.zip
+     xargs zip $OUT/libpng_improved_read_fuzzer_seed_corpus.zip
 
 cp $SRC/libpng/contrib/oss-fuzz/*.dict \
-   $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
+     $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
