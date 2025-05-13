9761066d8460dd13135e925b14bff6c7f30973f4
Octave Charrin <octave.charrin@gmail.com>
2025-05-13 17:00:50 +0200

    removed WRITE disable in build.sh

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -26,7 +26,6 @@
 cat scripts/pnglibconf.dfa | \
   sed -e "s/option STDIO/option STDIO disabled/" \
       -e "s/option WARNING /option WARNING disabled/" \
-      -e "s/option WRITE enables WRITE_INT_FUNCTIONS/option WRITE disabled/" \
 > scripts/pnglibconf.dfa.temp
 mv scripts/pnglibconf.dfa.temp scripts/pnglibconf.dfa
 
